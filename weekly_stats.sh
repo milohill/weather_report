32d0d85b8a6f015d568be93eb7dd5ee00a732197
milohill <116620023+milohill@users.noreply.github.com>
2024-03-04 20:38:19 +0900

    get max and min

--- weekly_stats.sh
+++ weekly_stats.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-tail -7 /Users/sanifred/work/code/projects/weather_report/synthetic_historical_fc_accuracy.tsv | cut -f6 > /Users/sanifred/work/code/projects/weather_report/scratch.txt
+tail -7 synthetic_historical_fc_accuracy.tsv | cut -f6 > scratch.txt
 
-history=($(echo $(cat /Users/sanifred/work/code/projects/weather_report/scratch.txt)))
+history=($(echo $(cat scratch.txt)))
+
+for el in ${history[@]}; do
+    echo ${el#-} >> abs.txt
+done
+
+max=$(cat abs.txt | head -1)
+min=$(cat abs.txt | tail -1)
+
+echo "Max: $max, min: $min"
